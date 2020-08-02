4c11068cffaa785b5ce601a30da59353f41c4eb5
creatica-soft <55069938+creatica-soft@users.noreply.github.com>
2020-08-02 21:18:05 +1200

    Update relay-station-check.sh

--- relay-station-check.sh
+++ relay-station-check.sh
@@ -5,12 +5,14 @@ LOGGING_DAEMON_IP="127.0.0.1"
 LOGGING_DAEMON_PORT="6666"
 DATETIME=`date`
 iw wlan0 station dump | grep -A17 ${STATION_MAC} > ${TMPFILE}
-AUTHORIZED=`grep authorized: ${TMPFILE} | awk -F":" '{print $2}' | tr -d "\t"`
+RX_BYTES=`grep "rx bytes:" ${TMPFILE} | awk -F":" '{print $2}' | tr -d "\t"`
+TX_BYTES=`grep "tx bytes:" ${TMPFILE} | awk -F":" '{print $2}' | tr -d "\t"`
+AUTHORIZED=`grep "authorized:" ${TMPFILE} | awk -F":" '{print $2}' | tr -d "\t"`
 AUTHENTICATED=`grep "authenticated:" ${TMPFILE} | awk -F":" '{print $2}' | tr -d "\t"`
 ASSOCIATED=`grep "associated:" ${TMPFILE} | awk -F":" '{print $2}' | tr -d "\t"`
 CONNECTED_TIME=`grep "connected time:" ${TMPFILE} | awk -F":" '{print $2}' | tr -d "\t"`
 if [[ "${AUTHORIZED}" == "yes" && "${AUTHENTICATED}" == "yes" && "${ASSOCIATED}" == "yes" ]]; then
-        echo "${DATETIME}: engine-room-fan station ${STATION_MAC} is connected for ${CONNECTED_TIME}" | nc -un -w 1 ${LOGGING_DAEMON_IP} ${LOGGING_DAEMON_PORT}
+        echo "${DATETIME}: engine-room-fan station ${STATION_MAC} is connected for ${CONNECTED_TIME}}, rx ${RX_BYTES} bytes, tx ${TX_BYTES} bytes" | nc -un -w 1 ${LOGGING_DAEMON_IP} ${LOGGING_DAEMON_PORT}
 else
         echo "${DATETIME}: engine-room-fan station ${STATION_MAC} is disconnected" | nc -un -w 1 ${LOGGING_DAEMON_IP} ${LOGGING_DAEMON_PORT}
 # see gpio tools - connect a beeper to GPIO 21 for audible alarm 
